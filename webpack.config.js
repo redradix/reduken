461abfdbbeb3b2cb40da204154e5f7ffc5209621
Miguel <miguel@redradix.com>
2018-02-05 12:33:33 +0100

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, ''),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['@babel/preset-env'],
+                    plugins: [require('@babel/plugin-proposal-object-rest-spread')]
+                }
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map'
+};
