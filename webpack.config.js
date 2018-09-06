cc6b46f8a3107e152cf033288a7efd5ac623f8c7
Miguel <hugo.hernandez.estevez.contractor@bbva.com>
2018-09-06 15:52:38 +0200

    modified webpack to add pagination module

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,35 @@
-var path = require('path');
-var webpack = require('webpack');
+var path = require('path')
+var webpack = require('webpack')
 
 module.exports = {
-    entry: {
-        set: './src/set/index.js',
-        list: './src/list/index.js',
-        entities: './src/entities/index.js',
-        hash: './src/hash/index.js',
-        main: './src/index.js'
-    },
-    output: {
-        path: path.resolve(__dirname, ''),
-        filename: '[name].js',
-        library: ["reduken", "[name]"],
-		libraryTarget: "umd"
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['@babel/preset-env'],
-                    plugins: [require('@babel/plugin-proposal-object-rest-spread')]
-                }
-            }
-        ]
-    },
-    stats: {
-        colors: true
-    },
-    devtool: 'source-map'
-};
+  entry: {
+    set: './src/set/index.js',
+    list: './src/list/index.js',
+    entities: './src/entities/index.js',
+    hash: './src/hash/index.js',
+    pagination: './src/pagination/index.js',
+    main: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, ''),
+    filename: '[name].js',
+    library: ['reduken', '[name]'],
+    libraryTarget: 'umd'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env'],
+          plugins: [require('@babel/plugin-proposal-object-rest-spread')]
+        }
+      }
+    ]
+  },
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map'
+}
