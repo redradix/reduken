5ce1e9a9360b3615e4ad98152641200bd6f0b509
Andres Martin <monkeyandres@protonmail.com>
2019-04-11 12:04:53 +0200

    Refactor and document

--- src/lib/buildReducer.js
+++ src/lib/buildReducer.js
@@ -1,7 +1,7 @@
+// buildReducer :: (State, {Function}) -> (State, Action) -> State
 export default function buildReducer(initialState, handlers) {
   return (state = initialState, action) => {
     const handler = handlers[action.type]
-    if (!handler) return state
-    return handler(state, action)
+    return handler ? handler(state, action) : state
   }
 }
