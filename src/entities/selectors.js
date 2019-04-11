e63f4270ef7720daf47ce3a9727f97276fab2423
Andres Martin <monkeyandres@protonmail.com>
2019-04-11 12:00:18 +0200

    Document with JSDoc

--- src/entities/selectors.js
+++ src/entities/selectors.js
@@ -2,22 +2,57 @@ import { curry, includes, toString, values } from 'ramda'
 
 const EMPTY_OBJECT = {}
 
-// root state selector
+/**
+ * Get all the entities as Object
+ *
+ * @param {object} state
+ * @returns {object}
+ */
 export const getEntities = curry(state => state.entities)
-// get all entities for domain as an object
-export const getDomain = curry(function(domain, state) {
-  return getEntities(state)[domain] || EMPTY_OBJECT
-})
-// get single entity from domain, by its id
-export const getById = curry(function(domain, id, state) {
-  return getDomain(domain, state)[id]
-})
-// get all entities as an array
+
+/**
+ * Get all entities for domain as an object
+ *
+ * @param {string} domain
+ * @param {object} state
+ * @returns {object}
+ */
+export const getDomain = curry(
+  (domain, state) => getEntities(state)[domain] || EMPTY_OBJECT
+)
+
+/**
+ * Get single entity from domain, by it's id
+ *
+ * @param {string} domain
+ * @param {string} id
+ * @param {object} state
+ * @returns {object}
+ */
+export const getById = curry(
+  (domain, id, state) => getDomain(domain, state)[id]
+)
+
+/**
+ * Get all entities for domain as an Array
+ *
+ * @param {string} domain
+ * @param {object} state The global state
+ * @returns {array}
+ */
 export const getAll = curry((domain, state) => {
   const entities = getDomain(domain, state)
   return values(entities)
 })
 
+/**
+ * Get the entities with specific ids in a domain
+ *
+ * @param {string} domain
+ * @param {array} ids
+ * @param {object} state The global state
+ * @returns {array}
+ */
 export const getSome = curry((domain, ids, state) => {
   const stringIds = ids.map(toString)
   const entities = getDomain(domain, state)
