5236425c657fc3d14c6c7837356b60af39b9af74
Andres Martin <monkeyandres@protonmail.com>
2019-04-23 14:23:36 +0200

    Refactor generate-declarations

--- generate-declarations.sh
+++ generate-declarations.sh
@@ -3,5 +3,5 @@ modules=(hash)
 
 for module in "${modules[@]}"
 do
-	npx tsc src/$module/*.ts -d --emitDeclarationOnly --outDir lib/$module/ --lib es6 && cat lib/$module/actions.d.ts lib/$module/selectors.d.ts >> lib/$module/index.d.ts
+	npx tsc src/$module/*.ts -d --emitDeclarationOnly --outDir lib/$module/ --lib es6 
 done
