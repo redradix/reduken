814a586e19728e9e138297b012e00691a5f88ff9
Andres Martin <monkeyandres@protonmail.com>
2019-04-24 09:29:26 +0200

    Curry pagination selectors

--- src/pagination/selectors.ts
+++ src/pagination/selectors.ts
@@ -1,19 +1,24 @@
-import { propOr } from 'ramda'
+import { propOr, curry } from 'ramda'
 
-export const getCurrentPage = (domain: string, state: object): number =>
-  propOr(1, ['pagination', domain, 'page'], state)
+export const getCurrentPage: (domain: string, state: object) => number = curry(
+  (domain, state) => propOr(1, ['pagination', domain, 'page'], state)
+)
 
-export const getTotal = (domain: string, state: object): number =>
-  propOr(0, ['pagination', domain, 'total'], state)
+export const getTotal: (domain: string, state: object) => number = curry(
+  (domain, state) => propOr(0, ['pagination', domain, 'total'], state)
+)
 
-export const getPerPage = (domain: string, state: object): number =>
-  propOr(1, ['pagination', domain, 'perPage'], state)
+export const getPerPage: (domain: string, state: object) => number = curry(
+  (domain, state) => propOr(1, ['pagination', domain, 'perPage'], state)
+)
 
-export const getTotalPages = (domain: string, state: object): number => {
-  const total = getTotal(domain, state)
-  const perPage = getPerPage(domain, state)
-  return Math.ceil(total / perPage)
-}
+export const getTotalPages: (domain: string, state: object) => number = curry(
+  (domain, state) => {
+    const total = getTotal(domain, state)
+    const perPage = getPerPage(domain, state)
+    return Math.ceil(total / perPage)
+  }
+)
 
 export const hasPage = (
   domain: string,
@@ -21,11 +26,14 @@ export const hasPage = (
   state: object
 ): boolean => pageN > 0 && pageN <= getTotalPages(domain, state)
 
-export const hasNextPage = (domain: string, state: object): boolean =>
-  hasPage(domain, getCurrentPage(domain, state) + 1, state)
+export const hasNextPage: (domain: string, state: object) => boolean = curry(
+  (domain, state) => hasPage(domain, getCurrentPage(domain, state) + 1, state)
+)
 
-export const hasPrevPage = (domain: string, state: object): boolean =>
-  hasPage(domain, getCurrentPage(domain, state) - 1, state)
+export const hasPrevPage: (domain: string, state: object) => boolean = curry(
+  (domain, state) => hasPage(domain, getCurrentPage(domain, state) - 1, state)
+)
 
-export const getResults = (domain: string, state: object): any[] =>
-  propOr(undefined, ['pagination', domain, 'records'], state)
+export const getResults: (domain: string, state: object) => any[] = curry(
+  (domain, state) => propOr(undefined, ['pagination', domain, 'records'], state)
+)
