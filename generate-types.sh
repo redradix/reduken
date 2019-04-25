0b76265efb81b2824ee2fd6e6a44eeb77691639b
Andres Martin <monkeyandres@protonmail.com>
2019-04-25 12:49:29 +0200

    Put es2017 lib for supporting includes method in arrays

--- generate-types.sh
+++ generate-types.sh
@@ -3,5 +3,5 @@ modules=(batch entities hash list pagination set requests)
 
 for module in "${modules[@]}"
 do
-	npx tsc src/$module/*.ts -d --emitDeclarationOnly --outDir lib/$module/ --lib es6 && echo "Generated types for $module"
+	npx tsc src/$module/*.ts -d --emitDeclarationOnly --outDir lib/$module/ --lib es6,es2017 && echo "Generated types for $module"
 done
