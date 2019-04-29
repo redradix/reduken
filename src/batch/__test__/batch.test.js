d66883daa88f1827e5c844fdc4776ae026b129a4
Andres Martin <monkeyandres@protonmail.com>
2019-04-29 10:06:41 +0200

    Fix batch tests

--- src/batch/__test__/batch.test.js
+++ src/batch/__test__/batch.test.js
@@ -2,31 +2,30 @@ import enableBatching from '../reducer'
 import { batchActions } from '../actions'
 
 // use with hash reducer and hash actions
-import { hash } from '../../index'
-import * as Actions from '../../hash/actions'
-import * as Selectors from '../../hash/selectors'
+import { hash as hashReducer } from '../../index'
+import * as hash from '../../hash/'
 
 describe('Batch Module', () => {
-  const reducer = enableBatching(hash)
+  const reducer = enableBatching(hashReducer)
 
   it('Batches single action under specified name', () => {
     const action = batchActions('single action', [
-      Actions.hset('test', ['foo'], 'bar')
+      hash.set('test', ['foo'], 'bar')
     ])
     const result = reducer(undefined, action)
 
     expect(action.type).toEqual('single action')
-    expect(Selectors.hget('test', ['foo'], { hash: result })).toEqual('bar')
+    expect(hash.getFromPath('test', ['foo'], { hash: result })).toEqual('bar')
   })
 
   it('Batches multiple action under specified name', () => {
     const action = batchActions('multiple actions', [
-      Actions.hset('test', ['foo'], 'bar'),
-      Actions.hset('test', ['foo2'], 'bar2'),
-      Actions.hset('test', ['foo3'], 'bar3')
+      hash.set('test', ['foo'], 'bar'),
+      hash.set('test', ['foo2'], 'bar2'),
+      hash.set('test', ['foo3'], 'bar3')
     ])
     const result = reducer(undefined, action)
-    const testDomain = Selectors.hgetall('test', { hash: result })
+    const testDomain = hash.getDomain('test', { hash: result })
 
     expect(action.type).toEqual('multiple actions')
     expect(testDomain.foo).toEqual('bar')
--- src/batch/reducer.js
+++ src/batch/reducer.js
@@ -1,6 +1,6 @@
 export default function enableBatching(reducer) {
   return function batchReducer(state, action) {
-    if (actions.meta && action.meta.isBatch) {
+    if (action.meta && action.meta.isBatch) {
       return action.payload.reduce(batchReducer, state)
     }
     return reducer(state, action)
