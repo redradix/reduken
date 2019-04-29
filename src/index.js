0628412510cf9d7de2ca911d4ca6820920009de8
Andres Martin <monkeyandres@protonmail.com>
2019-04-29 13:50:05 +0200

    Remofe setReducer from main export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-export { default as set } from './set/reducer'
 export { default as list } from './list/reducer'
 export { default as hash } from './hash/reducer'
 export { default as entities } from './entities/reducer'
