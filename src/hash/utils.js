2cb3e8d2cc30d294ecafc27bfedd8b2587f0b93b
Andres Martin <monkeyandres@protonmail.com>
2019-12-23 12:39:58 +0100

    Fix #25 hash/set now works well with numbers in path

--- src/hash/utils.js
+++ src/hash/utils.js
@@ -1,2 +1,5 @@
 export const alwaysArray = data => (Array.isArray(data) ? data : [data])
 
+// Object keys must be strings, if not assocPath creates
+// an array instead of an object.
+export const alwaysStringPath = path => path.map(String)
