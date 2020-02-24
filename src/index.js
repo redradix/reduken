9b3a25db2d549b7f9eb714cfd7cced981954cc7f
Afsoon <said.atrahouch@gmail.com>
2020-02-24 15:42:55 +0100

    Generate the source map of javascript files and recover the index js file, removed by accident

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export { default as list } from './list/reducer'
+export { default as hash } from './hash/reducer'
+export { default as entities } from './entities/reducer'
+export { default as requests } from './requests/reducer'
+export { default as enableBatching } from './batch/reducer'
