a3bc3247218ba61c640f1dda75641848ab333b09
Andres Martin <monkeyandres@protonmail.com>
2020-10-23 10:23:18 +0200

    Remove hash selectors using domain

--- src/hash/selectors.ts
+++ src/hash/selectors.ts
@@ -1,7 +1,5 @@
-import { path as getPath, curry, pathOr, keys, length, hasPath } from 'ramda'
+import { path as getPath, curry } from 'ramda'
 import { alwaysArray } from './utils'
-
-const EMPTY_OBJECT = {}
 
 /**
  * Get a single property from a hash
@@ -11,43 +9,4 @@ export const getFromPath: (
   state: object,
 ) => any = curry((path, state) => {
   return getPath(['hash',...alwaysArray(path)], state)
-})
-
-/**
- * Get all the domain content
- */
-export const getDomain: (domain: string, state: object) => any[] = curry(
-  (state) => {
-    return getPath(['hash'], state)
-  },
-)
-
-/**
- * Get the keys of the domain
- */
-export const getKeys: (domain: string, state: object) => string[] = curry(
-  (domain, state) => {
-    return keys(pathOr(EMPTY_OBJECT, ['hash', domain], state))
-  },
-)
-
-/**
- * Get the domain length
- */
-export const getDomainLength: (domain: string, state: object) => number = curry(
-  (domain, state) => {
-    return length(getKeys(domain, state))
-  },
-)
-
-/**
- * Returns if there's a value inside a domain
- * and keys
- */
-export const existInPath: (
-  domain: string,
-  path: string[],
-  state: object,
-) => boolean = curry((domain, path, state) => {
-  return hasPath(['hash', domain, ...alwaysArray(path)], state)
 })
